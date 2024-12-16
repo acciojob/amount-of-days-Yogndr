1253f99313942913dcd19bd7a078186b11453a0b
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-17 01:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfaYear(year) {
+	if(year%4!=0 || year==1900){
+		return 365;
+	}
+	return 366;
+	
+}
+
+let year=prompt("Enter a valid year ")
+daysOfaYear(year)
